917bc15842abd09495110bfcf66798db8b2397e4
Alexandre <alexandreguilngar@gmail.com>
2018-11-11 16:00:06 +0100

    added availableRanges

--- event.js
+++ event.js
@@ -1,6 +1,8 @@
 const Moment = require("moment");
 const MomentRange = require("moment-range");
 const moment = MomentRange.extendMoment(Moment);
+const lodash = require("lodash");
+const flatten = lodash.flatten;
 // This is the Event class
 // An event is either opening (available) or busy (intervention)
 // An opening event can be recurring (it means it repeats itself on the same day, same hours the next weeks)
@@ -11,6 +13,7 @@ const moment = MomentRange.extendMoment(Moment);
 const eventList = [];
 const openingRanges = [];
 const busyRanges = [];
+let availableRanges = [];
 
 const Event = function(opening, recurring, startDate, endDate, range) {
   this.opening = opening;
@@ -33,7 +36,22 @@ const Event = function(opening, recurring, startDate, endDate, range) {
 // Only "available" events should be displayed
 // You can use slots, or return the availabilities the way you want
 Event.prototype.availabilities = function(fromDate, toDate) {
+  console.log("availableRanges : ");
+  console.log(subtractRanges(openingRanges, busyRanges));
   return; //Something awesome;
+};
+
+const subtractRanges = (open, busy) => {
+  return flatten(
+    open.map(s => {
+      return flatten(busy).reduce(
+        (remaining, o) => {
+          return flatten(remaining.map(r => r.subtract(o)));
+        },
+        [s]
+      );
+    })
+  );
 };
 
 const simpleSum = (a, b) => a + b;
