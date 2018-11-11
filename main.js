859bad1007207e4a5aac06e89e30c74c81aefe8f
Alexandre <alexandreguilngar@gmail.com>
2018-11-11 15:06:53 +0100

    imported momentjs in main.js

--- main.js
+++ main.js
@@ -1,31 +1,36 @@
 // This file is the main file, it contains a mock declaration of all the events of the company
 const { Event, eventList } = require("./event");
+const Moment = require("moment");
+const MomentRange = require("moment-range");
+const moment = MomentRange.extendMoment(Moment);
 
 // AVAILABILITIES //
 // The company is available every monday, from 10:30 to 14:00, beginning the 2nd of July
-let startDate = new Date(2018, 6, 2, 10, 30);
-let endDate = new Date(2018, 6, 2, 14, 00);
+let startDate = moment(new Date(2018, 6, 2, 10, 30));
+let endDate = moment(new Date(2018, 6, 2, 14, 00));
 new Event(true, true, startDate, endDate);
 
 // The company is available on tuesday 3rd of july, from 11:30 to 16:00
-startDate = new Date(2018, 6, 3, 11, 30);
-endDate = new Date(2018, 6, 3, 16, 00);
+startDate = moment(new Date(2018, 6, 3, 11, 30));
+endDate = moment(new Date(2018, 6, 3, 16, 00));
 new Event(true, false, startDate, endDate);
 
 // INTERVENTIONS //
 // The company has an intervention scheduled tuesday 3rd of july, from 12:30 to 13:00
-startDate = new Date(2018, 6, 3, 12, 30);
-endDate = new Date(2018, 6, 3, 13, 00);
+startDate = moment(new Date(2018, 6, 3, 12, 30));
+endDate = moment(new Date(2018, 6, 3, 13, 00));
 new Event(false, false, startDate, endDate);
 
 // The company has an intervention scheduled tuesday 9th of july, from 13:30 to 14:00
-startDate = new Date(2018, 6, 9, 13, 30);
-endDate = new Date(2018, 6, 9, 14, 00);
+startDate = moment(new Date(2018, 6, 9, 13, 30));
+endDate = moment(new Date(2018, 6, 9, 14, 00));
 new Event(false, false, startDate, endDate);
 
 // MAIN RESPONSE //
-const fromDate = new Date(2018, 6, 3, 10, 00);
-const toDate = new Date(2018, 6, 15, 10, 00);
+// The resident wants to know availabilities from 3rd of July,
+// 10AM, to 15th of July, 10AM
+const fromDate = moment(new Date(2018, 6, 3, 10, 00));
+const toDate = moment(new Date(2018, 6, 15, 10, 00));
 Event.prototype.availabilities(fromDate, toDate);
 
 /*
