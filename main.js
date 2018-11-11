5629841ea00ee9e5140e80fe72cdaa7410aac2a0
Alexandre <alexandreguilngar@gmail.com>
2018-11-11 15:20:54 +0100

    fixed invalid number in dates (00)

--- main.js
+++ main.js
@@ -7,30 +7,30 @@ const moment = MomentRange.extendMoment(Moment);
 // AVAILABILITIES //
 // The company is available every monday, from 10:30 to 14:00, beginning the 2nd of July
 let startDate = moment(new Date(2018, 6, 2, 10, 30));
-let endDate = moment(new Date(2018, 6, 2, 14, 00));
+let endDate = moment(new Date(2018, 6, 2, 14, 0));
 new Event(true, true, startDate, endDate);
 
 // The company is available on tuesday 3rd of july, from 11:30 to 16:00
 startDate = moment(new Date(2018, 6, 3, 11, 30));
-endDate = moment(new Date(2018, 6, 3, 16, 00));
+endDate = moment(new Date(2018, 6, 3, 16, 0));
 new Event(true, false, startDate, endDate);
 
 // INTERVENTIONS //
 // The company has an intervention scheduled tuesday 3rd of july, from 12:30 to 13:00
 startDate = moment(new Date(2018, 6, 3, 12, 30));
-endDate = moment(new Date(2018, 6, 3, 13, 00));
+endDate = moment(new Date(2018, 6, 3, 13, 0));
 new Event(false, false, startDate, endDate);
 
 // The company has an intervention scheduled tuesday 9th of july, from 13:30 to 14:00
 startDate = moment(new Date(2018, 6, 9, 13, 30));
-endDate = moment(new Date(2018, 6, 9, 14, 00));
+endDate = moment(new Date(2018, 6, 9, 14, 0));
 new Event(false, false, startDate, endDate);
 
 // MAIN RESPONSE //
 // The resident wants to know availabilities from 3rd of July,
 // 10AM, to 15th of July, 10AM
-const fromDate = moment(new Date(2018, 6, 3, 10, 00));
-const toDate = moment(new Date(2018, 6, 15, 10, 00));
+const fromDate = moment(new Date(2018, 6, 3, 10, 0));
+const toDate = moment(new Date(2018, 6, 15, 10, 0));
 Event.prototype.availabilities(fromDate, toDate);
 
 /*
